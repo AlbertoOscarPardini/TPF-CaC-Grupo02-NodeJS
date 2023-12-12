f5ed45b86da5b1d2fe2acf4a2d199305af49591f
Santiago López <gabsant2001@gmail.com>
2023-12-12 20:04:41 -0300

    [STYLES] Added missing styles and html

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,15 +1,10 @@
-const fs=require("fs");
-
-const productosJson=JSON.parse(fs.readFileSync("./src/data/products.json","utf-8"));
+const fs =require("fs");
+const productosJson = JSON.parse(fs.readFileSync("./src/data/products.json","utf-8"));
 
 const mainControllers = {
     home: (req, res) => {
-        
-        
-        //res.render('home', { title: 'Home | Funkoshop'});
         res.render('home', { title: 'Home | Funkoshop',coleccion : productosJson });
     },
-    
     contact: (req, res) => res.render('shop/contact', { title: 'Contact | Funkoshop'}),
     about: (req, res) => res.render('shop/about', { title: 'About | Funkoshop'}),
     faqs: (req, res) => res.render('shop/faqs', { title: 'Faqs | Funkoshop'})

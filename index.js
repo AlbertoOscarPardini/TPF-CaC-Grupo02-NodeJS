799a6dea31a17add382ad84ca53f8b3b0bd2374b
Santiago López <gabsant2001@gmail.com>
2023-12-12 19:26:05 -0300

    [FEAT] Add route for not found page

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
 const adminRoutes = require('./src/routes/adminRoutes');
 const authRoutes = require('./src/routes/authRoutes');
+const errorHandlers = require('./src/utils/errorHandlers');
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.urlencoded({extended: true})); 
 
@@ -34,5 +35,9 @@ app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
 app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
+
+// Ruta para error
+
+app.use(errorHandlers.notFoundPage)
 
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}/home`))
